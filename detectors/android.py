a3fc75fd6a99fa543437901f2b424c893ed11d07
GeVaDeKo <geert157@gmail.com>
2026-08-06 14:07:37 +0200

    Delete detectors directory

--- detectors/android.py
+++
@@ -1,6 +0,0 @@
-def is_android_project(root):
-    return (
-        (root / "settings.gradle").exists()
-        or (root / "settings.gradle.kts").exists()
-        or (root / "app" / "src" / "main" / "AndroidManifest.xml").exists()
-    )
--- detectors/laravel.py
+++
@@ -1,6 +0,0 @@
-def is_laravel_project(root):
-    return(
-        (root / "artisan").exists()
-        and (root / "composer.json").exists()
-        and (root / "app").exists()
-    )
